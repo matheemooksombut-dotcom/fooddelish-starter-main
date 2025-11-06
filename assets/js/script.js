99a86ccff1af74c4668b2c534fcd953b85a21745
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-11-06 21:43:35 +0700

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,21 @@ document.querySelector('.hamberger-icon').addEventListener('click' , function(){
     document.querySelector('.menu').classList.toggle('show');
 });
 
-function submitdata (){
-    let FirstnameDOM = document.querySelector('input[name=Firstname]')
-    let LastnameDOM = document.querySelector('input[name=Lastname]')
-    let UsernameDOM = document.querySelector('input[name=Username]')
-    let PasswordDOM = document.querySelector('input[name=Password]')
-    let ConfrimpasswordDOM = document.querySelector('input[name=Confrime]')
+function submitdata() {
+  let FirstnameDOM = document.querySelector('input[name=Firstname]');
+  let LastnameDOM = document.querySelector('input[name=Lastname]');
+  let UsernameDOM = document.querySelector('input[name=Username]');
+  let PasswordDOM = document.querySelector('input[name=Password]');
+  let ConfirmDOM = document.querySelector('input[name=Confirm]');
+
+  let userData = {
+    Firstname: FirstnameDOM.value,
+    Lastname: LastnameDOM.value,
+    Username: UsernameDOM.value,
+    Password: PasswordDOM.value,
+    ConfirmPassword: ConfirmDOM.value
+  };
+
+  console.log('Submit:', userData);
+  alert(`✅ ข้อมูลถูกส่งแล้ว\nชื่อ: ${userData.Firstname}\nนามสกุล: ${userData.Lastname}`);
 }
-
-
-let userData = {
-   Firstname: FirstnameDOM.value,
-   Lastname: LastnameDOM.value,
-   Username: UsernameDOM.value,
-   Password: PasswordDOM.value,
-   Confrimpassword: ConfrimpasswordDOM.value
- 
-}
-
-console.log('Summit' , userData)
