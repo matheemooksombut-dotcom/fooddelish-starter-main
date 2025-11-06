77b630d8e89f1234e2047e7df80a5e654268e344
matheemooksombut-dotcom <matheemooksombut@gmail.com>
2025-11-06 21:41:33 +0700

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
 document.querySelector('.hamberger-icon').addEventListener('click' , function(){
     document.querySelector('.menu').classList.toggle('show');
 });
+
+function submitdata (){
+    let FirstnameDOM = document.querySelector('input[name=Firstname]')
+    let LastnameDOM = document.querySelector('input[name=Lastname]')
+    let UsernameDOM = document.querySelector('input[name=Username]')
+    let PasswordDOM = document.querySelector('input[name=Password]')
+    let ConfrimpasswordDOM = document.querySelector('input[name=Confrime]')
+}
+
+
+let userData = {
+   Firstname: FirstnameDOM.value,
+   Lastname: LastnameDOM.value,
+   Username: UsernameDOM.value,
+   Password: PasswordDOM.value,
+   Confrimpassword: ConfrimpasswordDOM.value
+ 
+}
+
+console.log('Summit' , userData)
